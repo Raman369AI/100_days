f7c8ef3e48d1ba6f5565805516c8a181eb05af6f
Raman369AI <110351568+Raman369AI@users.noreply.github.com>
2023-04-29 05:52:55 +0530

    Update d11.hm.cp.py
    modified

--- d11.hm.cp.py
+++ d11.hm.cp.py
@@ -1,3 +1,5 @@
+
+
 import random
 def add(l):
   a=0
@@ -16,83 +18,51 @@ while i<2:
   user_cards.append(random.choice(cards))
   computer_cards.append(random.choice(cards))
   i+=1
-key=False
-while key==False:
-    scores=[add(user_cards),add(computer_cards)]
-    if scores[0]==21:
+
+scores=[add(user_cards),add(computer_cards)]
+if scores[0]==21:
         print("You win, got a black jack")
-        key=True
-    elif scores[1]==21:
+        
+elif scores[1]==21:
         print("Computer win , got a black jack")
-        key=True
-    else:
+       
+else:
         print(f"your score is {scores[0]} and 1st computer card is {computer_cards[0]}")
-        key=True
 while True:
-  if scores[0]==21 or scores[1]==21:
+  if scores[0]==21 and scores[1]==21:
     break
   if scores[0]!=21 and scores[1]!=21:
-    direction=input("Do you want to continue? Yes or No\n").lower()
-    if direction=='yes':
+     direction=input("Do you want to continue? Yes or No\n").lower()
+     if direction=='yes':
         user_cards.append(random.choice(cards))
         print(f"user score is {add(user_cards)}")
+        
         if add(user_cards)>21:
-           print("Computer win as user score breached 21")
-           break
+         print("Computer win as user score breached 21")
+         break
         elif add(user_cards)==21:
            print("you win as score reached 21")
-           print(computer_cards) 
-           print(user_cards)
            break
         elif add(user_cards)<21:
             while (add(computer_cards)<=16):
                computer_cards.append(random.choice(cards))
-            if add(computer_cards)>21:
-                    print("you win as computer cards breached 21")
-                    print(computer_cards) 
-                    print(user_cards)
-                    break
-            elif add(computer_cards)==21:
-                    print("Computer win as computer cards reached 21")
-                    print(computer_cards) 
-                    print(user_cards)
-                    break
-            elif add(computer_cards)<21:
-                    if(add(user_cards)>add(computer_cards)):
-                        print("You win as your score greater than computer score")
-                        print(computer_cards) 
-                        print(user_cards)
-                        break
-                    elif add(user_cards)< add(computer_cards):
-                        print("Computer win as your score less than computer score")
-                        print(computer_cards) 
-                        print(user_cards)
-                        break
-                    else:
-                        print('draw baby')
-                        print(computer_cards) 
-                        print(user_cards)
-                        break
-            else:
-                    print('draw')
-                    print(computer_cards) 
-                    print(user_cards)
-                    break
-            break
-    elif direction=='no':
-        if add(user_cards)>add(computer_cards):
+            
+     elif direction=='no' and add(user_cards)>add(computer_cards):
           print("You win as your score greater")
           print(computer_cards) 
           print(user_cards)
           break
-        elif add(user_cards)<add(computer_cards):
+     elif direction=='no' and add(user_cards)<add(computer_cards):
           print("Computer win")
           print(computer_cards) 
           print(user_cards)
           break
-        else:
+     else:
           print('draw')
           print(computer_cards) 
           print(user_cards)
           break
-    break
+else:
+  pass
+
+    
